9ed2db8957ca9c08291ea87e2cc018098b95fc17
EE20B040 Sameer <89986653+sameeranjan22@users.noreply.github.com>
2023-03-22 08:37:54 +0000

    Latest

---
+++ templates/database.py
@@ -0,0 +1,3 @@
+from sqlalchemy import create_engine
+
+engine = create_engine("mysql+pymysql://c6g5llox7g6yztz2wxv8:pscale_pw_1egFmsamoCtD99OgsHdMjKC3Ggtc92GKUtOss7cpnl0@ap-south.connect.psdb.cloud/currect_shows?charset=utf8mb4")
